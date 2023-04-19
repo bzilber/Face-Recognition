0e38fe5a4b4fd8225b855a59c97aaab38290c8dd
Ben <bzilber@users.noreply.github.com>
2023-04-19 11:10:09 -0600

    added tachyons to style Navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Navigation = () => {
   return (
-    <nav>
-        <p>Sign Out</p>
+    <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+        <p className='f3 link dim black underline pa3 pointer'>Sign Out</p>
     </nav>
   )
 }

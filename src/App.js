e48359d8de7648b627254986c14ac9a3bcd8e8c5
Ben <bzilber@users.noreply.github.com>
2023-04-19 14:07:52 -0600

    ImageLinkForm build.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo.js';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+// import 'tachyons'; seems to not matter if placed here or index.js, so far..
 
 function App() {
   return (
     <div className="App">
       <Navigation/>
        <Logo/>
-       {/* <ImageLinkForm/>
-      <FaceRecognition/> */}
+      <ImageLinkForm/>
+      {/* <FaceRecognition/> */}
     </div>
   );
 }

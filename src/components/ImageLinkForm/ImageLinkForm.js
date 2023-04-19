a8c207b98b2d8439f33d4f34e5f0a8ca51f72e51
Ben <bzilber@users.noreply.github.com>
2023-04-19 14:09:33 -0600

    test of stage

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -10,7 +10,7 @@ const ImageLinkForm = () => {
         <div className='center'>
             <div className='form center pa4 br3 shadow-5'>
                 <input className='f4 pa2 w-70 center' type='text'/>
-                <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+                <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detecto</button>
             </div>
         </div>
     </div>

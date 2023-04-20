599ece6149c050a950165bff2be871dfa1eca1bd
Ben <bzilber@users.noreply.github.com>
2023-04-20 11:51:04 -0600

    installed particles-bg

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo.js';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 // import 'tachyons'; seems to not matter if placed here or index.js, so far..
+import ParticlesBg from 'particles-bg'
 
 function App() {
   return (
     <div className="App">
+      <ParticlesBg type="circle" bg={true} />
       <Navigation/>
        <Logo/>
       <ImageLinkForm/>

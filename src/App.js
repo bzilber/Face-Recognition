4bef4b1517bf9b8d35649e995359e7c3784bad0c
Ben <bzilber@users.noreply.github.com>
2023-04-23 12:12:15 -0600

    .

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Component } from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo.js';
@@ -5,16 +6,26 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 // import 'tachyons'; seems to not matter if placed here or index.js, so far..
 import ParticlesBg from 'particles-bg'
 
-function App() {
+class App extends Component {
+  state = { input: '' };
+  
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onSubmit = () => {console.log('click')}
+
+  render = () => {
   return (
     <div className="App">
       <ParticlesBg type="circle" bg={true} />
       <Navigation/>
        <Logo/>
-      <ImageLinkForm/>
+      <ImageLinkForm onInputChange={this.onInputChange} />
       {/* <FaceRecognition/> */}
     </div>
   );
+  };
 }
 
 export default App;

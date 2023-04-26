3e2d7a7c72487597d0112a90423b34344dba05e7
Ben <bzilber@users.noreply.github.com>
2023-04-26 12:28:50 -0600

    PUT http://localhost:3000/image [HTTP/1.1 404 Not Found

--- src/App.js
+++ src/App.js
@@ -5,15 +5,87 @@ import Logo from './components/Logo/Logo.js';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 // import 'tachyons'; seems to not matter if placed here or index.js, so far..
 import ParticlesBg from 'particles-bg'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
+
+const returnClarifaiRequestOptions = (imageUrl) => {
+    // Your PAT (Personal Access Token) can be found in the portal under Authentification
+    const PAT = '40cbae2159f94ecf99023945c7b8be9f';
+    // Specify the correct user_id/app_id pairings
+    // Since you're making inferences outside your app's scope
+    const USER_ID = 'bzilber';       
+    const APP_ID = 'my-first-application';
+    // Change these to whatever model and image URL you want to use
+    const MODEL_ID = 'face-detection';
+    const IMAGE_URL = imageUrl;
+
+    const raw = JSON.stringify({
+      "user_app_id": {
+          "user_id": USER_ID,
+          "app_id": APP_ID
+      },
+      "inputs": [
+          {
+              "data": {
+                  "image": {
+                      "url": IMAGE_URL
+                  }
+              }
+          }
+      ]
+  });
+  
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Key ' + PAT
+    },
+    body: raw
+  };
+
+  return requestOptions;
+}
 
 class App extends Component {
-  state = { input: '' };
+  constructor(){
+    super();
+    this.state = { 
+      input: '',
+      imageUrl: '', 
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
+    }
+  }
   
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({input: event.target.value});
   }
 
-  onSubmit = () => {console.log('clicko9');}
+  onSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    
+    fetch("https://api.clarifai.com/v2/models/" + "face-detection" + "/outputs", returnClarifaiRequestOptions(this.state.input))
+        .then(response => response.json())
+        .then(response => {
+          console.log('hi, response')
+          if (response){
+            fetch('http://localhost:3000/image', {
+              method: 'put',
+              headers: {'Content-Type': 'application/json'},
+              body: JSON.stringify({
+                id: this.state.user.id
+              })
+            })
+              
+          }
+          
+        })
+  }
 
   render = () => {
   return (
@@ -25,10 +97,13 @@ class App extends Component {
         onInputChange={this.onInputChange} 
         onSubmit={this.onSubmit} 
       />
-      {/* <FaceRecognition/> */}
+      <FaceRecognition/>
     </div>
   );
   };
 }
 
 export default App;
+
+
+//practice api calls (and compare and contrast to these PAT authentication calls)
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -10,9 +10,9 @@ const ImageLinkForm = ({onInputChange, onSubmit}) => {
         <div className='center'>
             <div className='form center pa4 br3 shadow-5'>
                 <input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange}/>
-                <button className='w-30 grow f4 link ph3 pv2 dib    white bg-light-purple' 
+                <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple' 
                   onClick={onSubmit} 
-                >Detect</button>C
+                >Detect</button>
             </div>
         </div>
     </div>
